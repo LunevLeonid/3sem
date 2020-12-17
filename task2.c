6f73c9c39f2a994011c6363d41ba1b1d12f363c2
LunevLeonid <76067209+LunevLeonid@users.noreply.github.com>
2020-12-17 20:21:45 +0300

    Update task2.c

--- task2.c
+++ task2.c
@@ -8,28 +8,28 @@
 #define N 512
 
 int main(int argc, char* argv[]) {
-	if (argc != 3) {
-		printf("Incorrect usage\n");
-		return 1;
-	}
+    if (argc != 3) {
+        printf("Incorrect usage\n");
+        return 1;
+    }
 
     int fd = open(argv[1], O_CREAT|O_WRONLY, S_IWUSR); //argv[1] - name of the file
     
     ssize_t len = strlen(argv[2]);
     ssize_t i = 0;
     while (i < len) {
-    	ssize_t count = write(fd, argv[2] + i, len); //argv[2] - text that we write to the file
-    	if (count < 0) {
-    		perror("Failed to write\n");
-    		return 2;
-    	}
-    	i += count;
+        ssize_t count = write(fd, argv[2] + i, len); //argv[2] - text that we write to the file
+        if (count < 0) {
+            perror("Failed to write\n");
+            return 2;
+        }
+        i += count;
     }
     
     int c = close(fd);
     if (c < 0) {
-    	perror("Failed to close\n");
-    	return 2;
+        perror("Failed to close\n");
+        return 2;
     }
     
     return 0;
