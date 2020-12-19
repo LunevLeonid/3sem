001e1d8fc9a35707a2cbd8ffa31dd2d55e4f57fc
LunevLeonid <76067209+LunevLeonid@users.noreply.github.com>
2020-12-19 10:44:00 +0300

    Update task13.c

--- task13.c
+++ task13.c
@@ -35,7 +35,7 @@ int main(int argc, char argv[]) {
 			print_process_info();
 			break;
 		default:
-			wait(NULL);
+			wait(0);
 			printf("Parent process:\n");
 			print_process_info();
 			break;
