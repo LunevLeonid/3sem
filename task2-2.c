1a4579e89811881fc7e2cada0c8db39aa9325856
LunevLeonid <76067209+LunevLeonid@users.noreply.github.com>
2020-12-19 21:29:30 +0300

    Update task2-2.c

--- task2-2.c
+++ task2-2.c
@@ -7,32 +7,29 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        printf("Incorrect usage\n");
+        printf("Usage: %s somefile str\n", argv[0]);
         return 1;
     }
 
-    int fd = open(argv[1], O_CREAT|O_WRONLY, S_IWUSR); //argv[1] - name of the file
+    int fd = open(argv[1], O_CREAT|O_WRONLY, S_IRUSR | S_IWUSR); //argv[1] - name of the file
     if (fd < 0) {
         perror("Failed to open");
         return 2;
     }
-    
-    int len = strlen(argv[2]);
+
+    size_t len = strlen(argv[2]);
     int count = dprintf(fd, "%s\n", argv[2]); //argv[2] - text to write
-    if (count < 0) {
+    if (ftruncate(fd, (long int)len) < 0) {
+        perror("Failed to ftruncate");   
+    } else if (count < 0) {
         perror("Failed to write");
-        return 3;
-    }
-    if (count < len) {
+    } else if (count < len) {
         printf("Unsuccessful write");
-        return 4;
     }
-    
-    int c = close(fd);
-    if (c < 0) {
+
+    if (close(fd) < 0) {
         perror("Failed to close\n");
         return 5;
     }
-    
     return 0;
 }
