28a181cc54c3a71326a3b3ff96b510e08ff23710
LunevLeonid <76067209+LunevLeonid@users.noreply.github.com>
2020-12-19 13:57:56 +0300

    Update task9.c

--- task9.c
+++ task9.c
@@ -6,12 +6,17 @@
 #include <sys/vfs.h>
 
 int main(int argc, char* argv[]) {
-	struct statfs stfs_buf;
-	if (statfs(".", &stfs_buf) == -1) {
-		perror("Unable to statfs");
+	if (argc != 2) {
+		printf("Usage: %s somefile\n", argv[0]);
 		return 1;
 	}
-	
+
+	struct statvfs stfs_buf;
+	if (statvfs(argv[1], &stfs_buf) == -1) {
+		perror("Failed to statvfs");
+		return 1;
+	}
+
 	printf("Total space:  %ld B\n", stfs_buf.f_blocks * stfs_buf.f_frsize);
 	printf("Free space:  %ld B\n", stfs_buf.f_bfree * stfs_buf.f_frsize);
 	printf("Available space:  %ld B\n", stfs_buf.f_bavail * stfs_buf.f_frsize);
