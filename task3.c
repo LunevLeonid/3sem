28745672524aa180b015e669bd574e946eeaa0ba
LunevLeonid <76067209+LunevLeonid@users.noreply.github.com>
2020-12-19 22:33:32 +0300

    Update task3.c

--- task3.c
+++ task3.c
@@ -8,47 +8,39 @@
 
 int main(int argc, char* argv[]) {
 	if (argc != 3) {
-		printf("Incorrect usage\n");
+		printf("Usage: %s readfile writefile\n", argv[0]);
 		return 1;
 	}
-	
-	struct stat st_buf1;
-	struct stat st_buf2;
-	if (lstat(argv[1], &st_buf1) == -1) {
+
+	struct stat st_buf;
+	if (lstat(argv[1], &st_buf) == -1) {
 		perror("Failed to stat");
 		return 2;
 	}
-	if (lstat(argv[2], &st_buf2) == -1) {
-		perror("Failed to stat");
-		return 2;
-	}
-	if (((st_buf1.st_mode) & (S_IFMT)) != (S_IFREG)) {
+	if (S_ISREG(st_buf.st_mode)) {
 		printf("Not regular read file\n");
 		return 3;
 	}
-	if (((st_buf2.st_mode) & (S_IFMT)) != (S_IFREG)) {
-		printf("Not regular write file\n");
-		return 4;
-	}
-	
+
 	char buf[N];
 	int fd_rd = open(argv[1], O_RDONLY); //argv[1] - file that we copy
 	if (fd_rd == -1) {
 		perror("Failed to open\n");
 		return 4;
 	}
-	
-	int fd_wr = open(argv[2], O_WRONLY); //argv[2] - file we copy to
+
+	int fd_wr = open(argv[2], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR); //argv[2] - file we copy to
 	if (fd_wr == -1) {
 		perror("Failed to open\n");
+		close(fd_rd);
 		return 5;
 	}
-	
+
 	while (1) {
 		ssize_t count_read = read(fd_rd, buf, N);
 		if (count_read == -1) {
-			perror("Failed to read\n");
-			return 6;
+			perror("Failed to read");
+			break;
 		}
 		if (count_read == 0) {
 			break;
@@ -56,23 +48,23 @@ int main(int argc, char* argv[]) {
 		
 		ssize_t i = 0;
 		while (i < count_read) {
-			ssize_t count_write = write(fd_wr, buf + i, count_read);
+			ssize_t count_write = write(fd_wr, buf + i, count_read - i);
 			if (count_write == -1) {
 				perror("Failed to write");
+				close(fd_wr);
+				close(fd_rd);
 				return 7;
 			}
 			i += count_write;
 		}
 	}
-	
+
 	if (close(fd_wr) < 0) {
 		perror("Failed to close");
-		return 8;
 	}
-	
+
 	if (close(fd_rd) < 0) {
 		perror("Failed to close");
-		return 9;
 	}
 	return 0;
 }
