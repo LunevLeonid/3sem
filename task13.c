0eb844a33f71345d6a56f211496009ad602fc000
LunevLeonid <76067209+LunevLeonid@users.noreply.github.com>
2020-12-19 14:31:46 +0300

    Update task13.c

--- task13.c
+++ task13.c
@@ -4,41 +4,42 @@
 #include <sys/types.h>
 #include <pwd.h>
 #include <grp.h>
+#include <sys/wait.h>
 
 void print_process_info() {
 	printf("Process id: %d\n", getpid());
 	printf("Process parent id: %d\n", getppid());
 	struct passwd *pwd = getpwuid(getuid());
 	if (pwd == NULL) {
-		perror("Failed to get user and group id");
+		perror("Failed to getpwuid");
 	} else {
 		printf("User id: %d\n", pwd->pw_uid);
-		printf("Group id: %d\n", pwd->pw_uid);
-		struct group *gr = getgrgid(pwd->gid);
+		printf("Group id: %d\n", pwd->pw_gid);
+		struct group *gr = getgrgid(pwd->pw_gid);
 		if (pwd == NULL) {
-			perror("Failed to get group name");
+			perror("Failed to getpwuid");
 		} else {
-			printf("Group name: %s\n", gr->gr_name);
-		}
+		printf("Group name: %s\n", gr->gr_name);
 	}
-	return;
+}
+return;
 }
 
 int main(int argc, char argv[]) {
 	pid_t child_id = fork();
 	switch (child_id) {
-		case -1:
-			perror("Failed to make a new process");
-			return 1;
-		case 0:
-			printf("Child process:\n");
-			print_process_info();
-			break;
-		default:
-			wait(0);
-			printf("Parent process:\n");
-			print_process_info();
-			break;
+	case -1:
+		perror("Failed to make a new process");
+		return 1;
+	case 0:
+		printf("Child process:\n");
+		print_process_info();
+		break;
+	default:
+		wait(0);
+		printf("Parent process:\n");
+		print_process_info();
+		break;
 	}
 	return 0;
 }
