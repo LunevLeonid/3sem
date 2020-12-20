aa2c1dfac91e987de3a940047cd805494bcd6962
LunevLeonid <76067209+LunevLeonid@users.noreply.github.com>
2020-12-20 12:59:47 +0300

    Update task4.c

--- task4.c
+++ task4.c
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
 		printf("Usage: %s readfile writefile\n", argv[0]);
 		return 1;
 	}
-	
+
 	struct stat st_buf1;
 	if (lstat(argv[1], &st_buf1) == -1) {
 		perror("Failed to stat");
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
 		printf("Not regular read file\n");
 		return 3;
 	}
-	
+
 	char buf[N];
 	int fd_rd = open(argv[1], O_RDONLY);
 	if (fd_rd == -1) {
@@ -33,7 +33,7 @@ int main(int argc, char* argv[]) {
 		perror("Failed to open\n");
 		return 5;
 	}
-	
+
 	while (1) {
 		ssize_t count_read = read(fd_rd, buf, N);
 		if (count_read == -1) {
@@ -51,27 +51,27 @@ int main(int argc, char* argv[]) {
 			ssize_t count_write = write(fd_wr, buf + i, count_read - i);
 			if (count_write == -1) {
 				perror("Failed to write");
+				close(fd_wr);
+				close(fd_rd);
 				return 7;
 			}
 			i += count_write;
 		}
 	}
-	
+
 	struct timespec om_time[2] = {st_buf1.st_atim, st_buf1.st_mtime};
 	
 	if (fchmod(fd_wr, st_buf1.st_mode) < 0) {
 		perror("Failed to change mode");
-		return 8;
 	}
 	if (futimens(fd_wr, om_time) < 0) {
 		perror("Failed to futimens");
-		return 9;
 	}
-	
+
 	if (close(fd_wr) < 0) {
 		perror("Failed to close");
 	}
-	
+
 	if (close(fd_rd) < 0) {
 		perror("Failed to close");
 	}
