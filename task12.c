5185585d0f8912396735de12bc6d19a79d928365
LunevLeonid <76067209+LunevLeonid@users.noreply.github.com>
2020-12-21 10:32:37 +0300

    Update task12.c

--- task12.c
+++ task12.c
@@ -5,23 +5,28 @@
 #include <pwd.h>
 #include <grp.h>
 
+
 int main(int argc, char* argv[]) {
 	printf("Process id: %d\n", getpid());
 	printf("Process parent id: %d\n", getppid());
-	struct passwd *pwd = getpwuid(getuid());
-	if (pwd == NULL) {
-		perror("Failed to getpwuid");
-		return 1;
+	printf("User id: %d\n", getuid());
+	printf("Group id: %d\n", getgid());
+	
+	int ngroups_max = sysconf(_SC_NGROUPS_MAX);
+	gid_t *gr = (gid_t*)calloc(ngroups_max, sizeof(gid_t));
+	
+	int ngroups = getgroups(ngroups_max, gr);
+	printf("Process groups:\n");
+	for (int i = 0; i < ngroups; i++) {
+		printf("%d", gr[i]);
+		struct group *grg = getgrgid(gr[i]);
+		if (grg == NULL) {
+			perror("Failed to getgrgid");
+		} else {
+			printf("  %s\n", grg->gr_name);
+		}
 	}
-	printf("User id: %d\n", pwd->pw_uid);
-	printf("User name: %s\n", pwd->pw_name);
-	printf("Group id: %d\n", pwd->pw_gid);
-	struct group *pgr = getgrgid(pwd->pw_gid);
-	if (pgr == NULL) {
-		perror("Failed to get group");
-		return 1;
-	}
-	printf("Group name: %s\n", pgr->gr_name);
+	
+	free(gr);
 	return 0;
 }
-
